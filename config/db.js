de48e0f8d126edd942afa39bee8978d59860ee68
linaG <linagiradlot@hotmail.com>
2024-08-05 23:26:46 -0500

    primer commit

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('userAuth', 'root', 'password', {
-  host: 'localhost',
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql'
 });
 
